b47de6ea577d6be8919f7a319bed39ab418e802b
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-04-17 08:58:27 +0300

    Complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new header element with an id of 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Set the text content of the new header element
+const yourName = 'Andrew Nzioki'; // Replace with your own name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Add the new header element to the document
+const body = document.querySelector('body');
+body.appendChild(newHeader);
+// Set the nodeName property of the new element
